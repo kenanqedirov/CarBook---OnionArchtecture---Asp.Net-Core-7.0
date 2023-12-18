c2be97093a530347df320f7f0682a56f79348d0c
kenanqedirov <k.qedirov1999@gmail.com>
2023-12-18 23:10:42 +0400

    Add project files.

---
+++ Core/Domain/Entities/Brand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+
+
+        public List<Car> Cars { get; set; }
+    }
+}
